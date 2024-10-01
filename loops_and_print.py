8b8519b1f523ef5e417a2fea60f70bcf4c60498a
msolfritz <msolfritz@gmail.com>
2024-10-01 17:30:04 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,17 @@
-def enumerate_list(list):
-    return list
-
+def enumerate_list(colors):
+    lista = []
+    for index, s in enumerate(colors):
+        if s:
+           lista.append(f"{len(lista)}. {s}")
+    return lista
+colors = ["Red", "Green", "", "White", "Black"]
+print (enumerate_list(colors))
 
 def enumerate_backwards(list):
-    return list
+    lista = []
+    for index, s in enumerate(list):
+       if s:
+          lista.append(f"{len(lista)}. {s[::-1]}")
+    return lista
+colors = ["Red", "Green", "", "White", "Black"]
+print(enumerate_backwards(colors))
